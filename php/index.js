9a262e7dae78c42901a0847ab6002e53dbd1ebdb
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-11-28 21:11:08 +0200

    add

--- php/index.js
+++ php/index.js
@@ -1,12 +1,3 @@
-// document.querySelectorAll('input[type="checkbox"]').forEach(function(checkbox) {
-//     checkbox.addEventListener('change', function() {
-//         if (this.checked) {
-//             this.parentNode.classList.add('checked');
-//         } else {
-//             this.parentNode.classList.remove('checked');
-//         }
-//     });
-// });
 
 document.querySelectorAll('.master-checkbox-input').forEach(function(checkbox) {
     checkbox.addEventListener('change', function() {
